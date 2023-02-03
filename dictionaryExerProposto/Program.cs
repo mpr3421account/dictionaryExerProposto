1b9ef6d33196f5754d3352615040f10728e8bfca
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-03 17:47:16 -0300

    Final

--- dictionaryExerProposto/Program.cs
+++ dictionaryExerProposto/Program.cs
@@ -5,3 +5,39 @@ fazer um programa para ler os registros de votação a partir de um
 arquivo, e daí gerar um relatório consolidado com os totais de cada
 candidato.*/
 
+
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        while (!sr.EndOfStream)
+        {
+            string[] votingRecord = sr.ReadLine().Split(',');
+            string name = votingRecord[0];
+            int votes = int.Parse(votingRecord[1]);
+
+            if (dictionary.ContainsKey(name))
+            {
+                dictionary[name] += votes;
+            }
+            else
+            {
+                dictionary[name] = votes;
+            }
+        }
+        foreach(var item in dictionary)
+        {
+            Console.WriteLine(item.Key + ": " + item.Value);
+        }
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("An error occurred: " + e.Message);
+}
